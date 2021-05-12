6e41112429c04d6fb4e41e660805ba71f76c4e68
alexander eades <alexander.eades@student.centralia.edu>
2021-05-12 08:05:48 -0700

    Add project files.

---
+++ Eades_Dialer/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+// Alexander Eades
+// IT112
+// NOTES: notes the instructor should read
+// BEHAVIORS NOT IMPLIMENTED AND WHY: 
+namespace Eades_Dialer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("dialing");
+        }
+    }
+}
